a751b84058e93eab76c030a2d843d0c22cd1b55c
Aniruddh Singh <aniruddhsinghindia@gmail.com>
2023-04-17 14:57:20 +0530

    sdf

--- namaste.js
+++ namaste.js
@@ -2,4 +2,7 @@ Hello everyone my name is aniruddh singh
 
 Today a standup comedian named Samay Raina came in our college Harcourt Butler Technical University.
 
-git pull request.
+// git pull request.
+
+
+hey whatsapp
