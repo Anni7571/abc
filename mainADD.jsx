1c7761dfbb51cb4cb8477a3258c5f70ce9a03ad2
Aniruddh Singh <aniruddhsinghindia@gmail.com>
2023-04-17 14:56:22 +0530

    sdf

--- mainADD.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const mainADD = () => {
-    return <div>new file added</div>;
-};
-
-export default mainADD;
--- newOne.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const newOne = () => {
-    return <div>namaste</div>;
-};
-
-export default newOne;
